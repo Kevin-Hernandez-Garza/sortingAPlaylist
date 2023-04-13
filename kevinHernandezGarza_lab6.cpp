3d3899c17a9399654db864a78599b75a90087d9b
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2023-04-12 23:24:43 -0500

    setting up source file

--- kevinHernandezGarza_lab6.cpp
+++ kevinHernandezGarza_lab6.cpp
@@ -6,15 +6,65 @@
 // Due Date: 4/15/23
 // Instructor: Dr.Haytham Mohamed
 //
-// This program asks the user to input 10 of their favorite songs along with the artists names, then an algorithm sorts the playlist according to the artist name.
+// This program asks the user to input 10 of their favorite songs along with the artists names
+// then an algorithm sorts the playlist according to the artist name.
 //  ******************************************************************
 
-#include <iostream>
+/*
+You will write a program that allows the user to enter 10 songs and their artists into
+a playlist and will sort the entries by artist order.  The program should use a 10 element
+array of structure objects that holds two string variables:  artist and title.  Once the data
+has been read in and stored, it should sort the entries in ascending order by artist.
+It should then display the sorted array with appropriate headings.
 
+
+Modularity:   The main function should create the Playlist object and call at least three other functions:
+
+
+1.	Get the data from the user. Using a loop, have the user populate the playlist object array with
+10 artist and songs in no particular order.   No validation is necessary.   However, you should use
+the C++ getline function instead of cin so that the user can type spaces in the name and song title.
+
+
+2.	Sort the list.  You may use either the bubble sort or the selection sort to sort your playlist.
+
+
+3.	Display the list.  The function should display the artist and title in sorted order with appropriate headings.
+
+Sample Output:
+Playlist
+Artist			Title
+======================================
+Cream			Sunshine of Your Love
+Jimi Hendrix		All Along the Watchtower
+Stevie Ray Vaughn	Texas Flood
+ZZ Top			La Grange
+*/
+
+#include <iostream>
 using namespace std;
+
+void getData();
+void sortList();
+void displayList();
 
 int main()
 {
 
     return 0;
+}
+
+void getData()
+{
+    // BODY
+}
+
+void sortList()
+{
+    // BODY
+}
+
+void displayList()
+{
+    // BODY
 }
