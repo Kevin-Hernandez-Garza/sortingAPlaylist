9c5b51019e08bf9e40c2a1508ae5c6b367beddbb
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2023-05-05 15:50:58 -0500

    deleted unnecessary comments

--- kevinHernandezGarza_lab6.cpp
+++ kevinHernandezGarza_lab6.cpp
@@ -1,11 +1,3 @@
-//******************************************************************
-// Author: Kevin Hernandez-Garza
-// Assignment Number: Programming Assignment "6"
-// File Name: KevinHernandezG_Lab4.cpp
-// Course/Section: COSC 1337 Section -009
-// Due Date: 4/15/23
-// Instructor: Dr.Haytham Mohamed
-//
 // This program asks the user to input 10 of their favorite songs along with the artists names
 // then an algorithm sorts the playlist according to the artist name.
 //  ******************************************************************
